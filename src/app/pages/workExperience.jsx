3fa48bb3253b3d7587d56834714980abd3f18b93
Quinn Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-11-02 09:16:02 -0400

    Centered work experience for large screens

--- src/app/pages/workExperience.jsx
+++ src/app/pages/workExperience.jsx
@@ -5,93 +5,99 @@ const WorkExperience = () => {
         <div className="font-bold font-sans text-3xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy text-right">
           Work Experience
         </div>
-        <div>
-          <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
-            SC Strategic Solutions
+        <div className="ml-auto lg:mx-auto w-fit">
+          <div>
+            <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
+              SC Strategic Solutions
+            </div>
+            <div className="text-lg text-slate-600">
+              Quality Assurance Specialist
+            </div>
+            <div className="text-sm text-slate-600">October 2023 - Present</div>
+            <ul className="list-disc p-2 pl-5">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
           </div>
-          <div className="text-lg text-slate-600">
-            Quality Assurance Specialist
+          <div>
+            <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
+              Franklin Monument
+            </div>
+            <div className="text-lg text-slate-600">
+              Freelance Frontend Developer
+            </div>
+            <div className="text-sm text-slate-600">
+              June 2023 - August 2023
+            </div>
+            <ul className="list-disc p-2 pl-5">
+              <li>
+                Utilized React, TypeScript, and Tailwind CSS for a web
+                application for order processing.
+              </li>
+              <li>
+                Designed and developed responsive UIs using various React
+                libraries.
+              </li>
+              <li>
+                Collaborated as a team to solve problems while maintaining a
+                cohesive work environment.
+              </li>
+              <li>Designed data-driven analytical dashboards.</li>
+            </ul>
           </div>
-          <div className="text-sm text-slate-600">October 2023 - Present</div>
-          <ul className="list-disc p-2 pl-5">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <div>
-          <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
-            Franklin Monument
+          <div>
+            <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
+              Norwalk High School
+            </div>
+            <div className="text-lg text-slate-600">
+              Long-Term Substitute Teacher
+            </div>
+            <div className="text-sm text-slate-600">
+              December 2022 - June 2023
+            </div>
+            <ul className="list-disc p-2 pl-5">
+              <li>
+                AP Computer Science, Computer Science Principles, Algebra II
+                Teacher.
+              </li>
+              <li>Utilized Java and Python to write programs.</li>
+              <li>Taught material aligned with the APCS curriculum.</li>
+              <li>Created lesson plans and differentiated instruction.</li>
+            </ul>
           </div>
-          <div className="text-lg text-slate-600">
-            Freelance Frontend Developer
+          <div>
+            <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
+              CloudCME
+            </div>
+            <div className="text-lg text-slate-600">
+              Software Developer Intern
+            </div>
+            <div className="text-sm text-slate-600">
+              January 2020 - May 2022
+            </div>
+            <ul className="list-disc pl-5 p-2">
+              <li>Completed multiple project assignments in VB.net.</li>
+              <li>
+                Learned and worked in an agile environment incorporating JIRA
+                and Asana.
+              </li>
+              <li>
+                Worked with Microsoft SQL Server Management Studio to create and
+                modify stored procedures.
+              </li>
+              <li>Imported and created custom templates using Frevvo forms.</li>
+              <li>
+                Researched and developed coding projects and presented a
+                technical presentation for implementation.
+              </li>
+              <li>
+                Tested code and sites as part of the Quality Control process for
+                implementation to a production environment.
+              </li>
+            </ul>
           </div>
-          <div className="text-sm text-slate-600">June 2023 - August 2023</div>
-          <ul className="list-disc p-2 pl-5">
-            <li>
-              Utilized React, TypeScript, and Tailwind CSS for a web application
-              for order processing.
-            </li>
-            <li>
-              Designed and developed responsive UIs using various React
-              libraries.
-            </li>
-            <li>
-              Collaborated as a team to solve problems while maintaining a
-              cohesive work environment.
-            </li>
-            <li>Designed data-driven analytical dashboards.</li>
-          </ul>
-        </div>
-        <div>
-          <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
-            Norwalk High School
-          </div>
-          <div className="text-lg text-slate-600">
-            Long-Term Substitute Teacher
-          </div>
-          <div className="text-sm text-slate-600">
-            December 2022 - June 2023
-          </div>
-          <ul className="list-disc p-2 pl-5">
-            <li>
-              AP Computer Science, Computer Science Principles, Algebra II
-              Teacher.
-            </li>
-            <li>Utilized Java and Python to write programs.</li>
-            <li>Taught material aligned with the APCS curriculum.</li>
-            <li>Created lesson plans and differentiated instruction.</li>
-          </ul>
-        </div>
-        <div>
-          <div className="font-bold text-2xl bg-gradient-to-tr from-[#7E84F7] to-[#6FF78B] bg-clip-text text-transparent animate-gradient-xy mt-5">
-            CloudCME
-          </div>
-          <div className="text-lg text-slate-600">
-            Software Developer Intern
-          </div>
-          <div className="text-sm text-slate-600">January 2020 - May 2022</div>
-          <ul className="list-disc pl-5 p-2">
-            <li>Completed multiple project assignments in VB.net.</li>
-            <li>
-              Learned and worked in an agile environment incorporating JIRA and
-              Asana.
-            </li>
-            <li>
-              Worked with Microsoft SQL Server Management Studio to create and
-              modify stored procedures.
-            </li>
-            <li>Imported and created custom templates using Frevvo forms.</li>
-            <li>
-              Researched and developed coding projects and presented a technical
-              presentation for implementation.
-            </li>
-            <li>
-              Tested code and sites as part of the Quality Control process for
-              implementation to a production environment.
-            </li>
-          </ul>
         </div>
       </div>
     </div>
